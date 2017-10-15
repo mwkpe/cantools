5ef9b4d144fdf728a45bd5300c06eebecfb7d0e0
peter <xe@kthx.de>
2017-10-16 00:11:50 +0200

    Add frames

--- cansim.cpp
+++ cansim.cpp
@@ -1,4 +1,4 @@
-/* A command line program for transmitting CAN frames over UDP
+/* CAN bus simulation for development purposes
  */
 
 
@@ -23,17 +23,89 @@ __attribute__((unused)) static void escape(void* p) { asm volatile("" : : "g"(p)
 __attribute__((unused)) static void clobber() { asm volatile("" : : : "memory"); }
 
 
+namespace
+{
+
+
+struct Frame_veh_state
+{
+  std::uint8_t crc;
+  std::uint8_t aliv : 4;
+  std::uint8_t velocity_0 : 4;
+  std::uint8_t velocity_1;
+  std::uint8_t velocity_2 : 2;
+  std::uint8_t wiper_position_0 : 6;
+  std::uint8_t wiper_position_1;
+  std::uint8_t wiper_position_2 : 2;
+};
+
+struct Frame_flux_state
+{
+  std::uint32_t power_level;
+  std::uint32_t dispersal_rate;
+};
+
+struct Frame_date_time
+{
+  std::uint8_t time_type : 2;
+  std::uint8_t year_0 : 6;
+  std::uint8_t year_1;
+  std::uint8_t month : 4;
+  std::uint8_t day_0 : 4;
+  std::uint8_t day_1 : 1;
+  std::uint8_t am_pm : 1;
+  std::uint8_t hour : 4;
+  std::uint8_t minute_0 : 2;
+  std::uint8_t minute_1 : 4;
+};
+
+
+}  // namespace
+
+
 void simulate(std::atomic<bool>& stop, std::string&& ip, std::uint16_t port)
 {
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Woverflow"
+
   // Example frames
-  can_frame vehicle{0};
-  vehicle.can_id = 0xC9;
-  vehicle.can_dlc = 4;
-  vehicle.data[0] = 0xBB;
-  vehicle.data[1] = 0xFF;
+  can_frame veh_state{0};
+  veh_state.can_id = 201;
+  veh_state.can_dlc = 8;
+  auto* veh_state_data = reinterpret_cast<Frame_veh_state*>(veh_state.data);
+  veh_state_data->crc = 0xFF;
+  veh_state_data->aliv = 0;
+  veh_state_data->velocity_0 = 8800;
+  veh_state_data->velocity_1 = 8800 >> 4;
+  veh_state_data->velocity_2 = 8800 >> 12;
+  veh_state_data->wiper_position_0 = 2122;
+  veh_state_data->wiper_position_1 = 2122 >> 4;
+  veh_state_data->wiper_position_2 = 2122 >> 12;
+
+  can_frame flux_state{0};
+  flux_state.can_id = 233;
+  flux_state.can_dlc = 6;
+  auto* flux_state_data = reinterpret_cast<Frame_flux_state*>(flux_state.data);
+  flux_state_data->power_level = 1210000000;
+  flux_state_data->dispersal_rate = 7743;
+
+  can_frame date_time{0};
+  date_time.can_id = 245;
+  date_time.can_dlc = 5;
+  auto* date_time_data = reinterpret_cast<Frame_date_time*>(date_time.data);
+  date_time_data->year_0 = 1955;
+  date_time_data->year_1 = 1955 >> 6;
+  date_time_data->month = 11;
+  date_time_data->day_0 = 5;
+  date_time_data->day_1 = 5 >> 4;
+  date_time_data->hour = 6;
+  date_time_data->minute_0 = 31;
+  date_time_data->minute_1 = 31 >> 2;
+
+#pragma GCC diagnostic pop
 
   can_frame radar{0};
-  radar.can_id = 0xCA;
+  radar.can_id = 402;
   radar.can_dlc = 4;
   // Motorola byte order
   // Pos 7, len 14 = 12317
@@ -45,8 +117,8 @@ void simulate(std::atomic<bool>& stop, std::string&& ip, std::uint16_t port)
   radar.data[3] = 0b0100'1011;
 
   // Network inferface and timer
-  udp::socket udp_socket;
-  util::timer timer;
+  udp::Socket udp_socket;
+  util::Timer timer;
   try {
     udp_socket.open(ip, port);
     timer.init_system_timer();
@@ -58,12 +130,19 @@ void simulate(std::atomic<bool>& stop, std::string&& ip, std::uint16_t port)
 
   // Transmit scheduling
   auto transmit = [&](std::uint64_t time_ms) {
-    if (time_ms % 500 == 0) {  // 500 ms cycle time
-      vehicle.data[0]++;
-      udp_socket.transmit(&vehicle);
+    if (time_ms % 250 == 0) {  // 250 ms cycle time
+      veh_state_data->aliv++;
+      // Add crc calculation
+      udp_socket.transmit(&veh_state);
     }
-    if (time_ms % 200 == 0) {  // 200 ms cycle time
+    if (time_ms % 100 == 0) {
+      udp_socket.transmit(&flux_state);
+    }
+    if (time_ms % 225 == 0) {
       udp_socket.transmit(&radar);
+    }
+    if (time_ms % 1333 == 0) {
+      udp_socket.transmit(&date_time);
     }
   };
 

06bef9dd28da1fa925391b3fb9c246935712ad39
peter <xe@kthx.de>
2017-10-20 17:18:06 +0200

    Rewrite code, change data string ordering

--- cantx.cpp
+++ cantx.cpp
@@ -13,7 +13,9 @@
 #include <ios>
 
 #include "cxxopts.hpp"
+
 #include "cansocket.h"
+#include "priority.h"
 
 
 can_frame build_frame(const std::string& id, const std::string& data)
@@ -24,12 +26,11 @@ can_frame build_frame(const std::string& id, const std::string& data)
   frame.can_dlc = (data.size() + 1) / 2;
   if (frame.can_dlc > CAN_MAX_DLC)
     frame.can_dlc = CAN_MAX_DLC;
+
   auto d = std::strtoull(data.c_str(), nullptr, 16);
-  // Reversing order of bytes so a 010203 string is 010203 in the data field
-  int n = 0;
-  int i = frame.can_dlc;
-  while (i--)
-    frame.data[i] = d >> n++ * 8;  // Place lowest byte using highest index
+  for (int i=0; i<frame.can_dlc; ++i)
+    frame.data[i] = d >> i * 8;
+
   return frame;
 }
 
@@ -38,7 +39,7 @@ void print_frame(const can_frame& frame)
 {
   std::cout << "id: " << std::hex << frame.can_id << std::dec << ", dlc: "
       << static_cast<int>(frame.can_dlc) << ", data: " << std::hex << std::setfill('0');
-  for (int i=0; i<frame.can_dlc; i++) {
+  for (int i=frame.can_dlc-1; i>=0; --i) {
     std::cout << std::setw(2) << static_cast<int>(frame.data[i]) << ' ';
   }
   std::cout << '\n';
@@ -71,31 +72,33 @@ void transmit_frame(std::atomic<bool>& transmit_cyclical, const std::string devi
 }
 
 
-std::tuple<std::string, can_frame, int> parse_args(int argc, char** argv)
+std::tuple<std::string, can_frame, int, bool> parse_args(int argc, char** argv)
 {
   std::string device;
   std::string id;
-  std::string data;
+  std::string payload;
   int cycle_time;
+  bool realtime = false;
 
   try {
     cxxopts::Options options{"cantx", "CAN message transmitter"};
     options.add_options()
-      ("id", "Hex message id", cxxopts::value<std::string>(id))
-      ("data", "Hex data string", cxxopts::value<std::string>(data)->default_value("00"))
-      ("cycle", "Cycle time in ms", cxxopts::value<int>(cycle_time)->default_value("-1"))
-      ("device", "CAN device name", cxxopts::value<std::string>(device)->default_value("can0"))
+      ("i,id", "Hex frame ID", cxxopts::value<std::string>(id))
+      ("p,payload", "Hex data string", cxxopts::value<std::string>(payload)->default_value("00"))
+      ("c,cycle", "Cycle time in ms", cxxopts::value<int>(cycle_time)->default_value("-1"))
+      ("d,device", "CAN device name", cxxopts::value<std::string>(device)->default_value("can0"))
+      ("r,realtime", "Enable realtime scheduling policy", cxxopts::value<bool>(realtime))
     ;
     options.parse(argc, argv);
 
     if (options.count("id") == 0) {
-      throw std::runtime_error{"Message ID must be specified, use --id option"};
+      throw std::runtime_error{"Message ID must be specified, use -i or --id option"};
     }
-    if (data.size() % 2 || data.size() > 16) {
-      throw std::runtime_error{"Data size error, size must be even and <= 16"};
+    if (payload.size() % 2 || payload.size() > 16) {
+      throw std::runtime_error{"Payload size error, size must be even and <= 16"};
     }
 
-    return std::make_tuple(std::move(device), build_frame(id, data), cycle_time);
+    return std::make_tuple(std::move(device), build_frame(id, payload), cycle_time, realtime);
   }
   catch (const cxxopts::OptionException& e) {
     throw std::runtime_error{e.what()};
@@ -108,9 +111,10 @@ int main(int argc, char** argv)
   std::string device;
   can_frame frame;
   int cycle_time;
+  bool realtime;
 
   try {
-    std::tie(device, frame, cycle_time) = parse_args(argc, argv);
+    std::tie(device, frame, cycle_time, realtime) = parse_args(argc, argv);
   }
   catch (const std::runtime_error& e) {
     std::cerr << "Error parsing command line options:\n" << e.what() << std::endl;
@@ -124,16 +128,22 @@ int main(int argc, char** argv)
     std::cout << " each " << cycle_time << " ms...";
   std::cout << '\n';
   print_frame(frame);
-  if (transmit_cyclical.load())
-    std::cout << "Press enter to stop..." << std::endl;
 
   std::thread transmitter{&transmit_frame, std::ref(transmit_cyclical), device, frame, cycle_time};
 
-  if (transmit_cyclical.load())
+  if (transmit_cyclical.load()) {
+    if (realtime) {
+      if (priority::set_realtime(transmitter.native_handle()))
+        std::cout << "Transmitter thread set to realtime scheduling policy\n";
+      else
+        std::cout << "Warning: Could not set scheduling policy, forgot sudo?\n";
+    }
+    std::cout << "Press enter to stop..." << std::endl;
     std::cin.ignore();  // Wait in main thread
+    std::cout << "Stopping transmitter..." << std::endl;
+    transmit_cyclical.store(false);
+  }
 
-  std::cout << "Stopping transmitter..." << std::endl;
-  transmit_cyclical.store(false);
   transmitter.join();
 
   std::cout << "Program finished" << std::endl;
